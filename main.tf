2ceb75a6fe50e9246ace62ad2071b53dce78d994
ahmedgmansour <60443603+ahmedgmansour@users.noreply.github.com>
2022-05-24 02:06:47 +0200

    a

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "aws_subnet" "public" {
   vpc_id                  = module.vpc.vpc_id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = "true"
-  availability_zone       = "us-east-1"
+  availability_zone       = "us-east-1a"
 
   tags = {
     Name = "main-public-1"
@@ -37,7 +37,7 @@ resource "aws_subnet" "private" {
   vpc_id                  = module.vpc.vpc_id
   cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = "false"
-  availability_zone       = "us-east-1"
+  availability_zone       = "us-east-1a"
 
   tags = {
     Name = "main-private-1"
