4c451379702083777987bb19a12b7f3ed0062aec
ahmedgmansour <60443603+ahmedgmansour@users.noreply.github.com>
2022-05-24 17:03:57 +0200

    instance.tf

---
+++ modules/vpc/instance.tf
@@ -0,0 +1,44 @@
+data "aws_ssm_parameter" "webserver-ami" {
+  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+}
+
+resource "aws_instance" "example" {
+  ami           = data.aws_ssm_parameter.webserver-ami.value
+  instance_type = "t2.micro"
+
+  # the VPC subnet
+  subnet_id = aws_subnet.main-public-1.id
+
+  # the security group
+  vpc_security_group_ids = [aws_security_group.allow-ssh.id]
+
+  # the public SSH key
+  #key_name = aws_key_pair.mykeypair.key_name
+  # provisioner
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum -y install httpd && sudo systemctl start httpd",
+      "echo '<h1><center>My Test Website With Help From Terraform Provisioner</center></h1>' > index.html",
+      "sudo mv index.html /var/www/html/"
+    ]
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = file("~/.ssh/id_rsa")
+      host        = self.public_ip
+    }
+  }
+  tags = {
+    Name = "webserver"
+  }
+}
+
+resource "aws_key_pair" "webserver-key" {
+  key_name   = "webserver-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+
+output "Webserver-Public-IP" {
+  value = aws_instance.example.public_ip
+}
